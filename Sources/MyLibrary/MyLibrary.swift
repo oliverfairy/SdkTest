3cb83652b88f9826998cb6a1f31cff77d56a79ae
Oliver An <131226876+oliverfairy@users.noreply.github.com>
2024-01-25 18:59:48 +0900

    initial commit

--- Sources/MyLibrary/MyLibrary.swift
+++ Sources/MyLibrary/MyLibrary.swift
@@ -1,2 +1,12 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+public class TestSdkClass {
+    public func sum(a: Int, b: Int) -> Int {
+        return a + b;
+    }
+g
+    public func greet(person: String) -> String {
+        let greeting = "Hello, " + person + "!"
+        return greeting
+    }
+}
