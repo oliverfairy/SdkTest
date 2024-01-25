933975d7cdfeb50ad737c6e5f86b58bc584d54e3
Oliver An <131226876+oliverfairy@users.noreply.github.com>
2024-01-25 19:21:10 +0900

    Syntax fix

--- Sources/MyLibrary/MyLibrary.swift
+++ Sources/MyLibrary/MyLibrary.swift
@@ -1,11 +1,11 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
 public class TestSdkClass {
-    public func sum(a: Int, b: Int) -> Int {
+    func sum(a: Int, b: Int) -> Int {
         return a + b;
     }
-g
-    public func greet(person: String) -> String {
+
+    func greet(person: String) -> String {
         let greeting = "Hello, " + person + "!"
         return greeting
     }
