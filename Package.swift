d769f6e3f13866a9971a3c42350a806a3c4e4290
Oliver An <131226876+oliverfairy@users.noreply.github.com>
2024-01-31 15:24:34 +0900

    wip

--- Package.swift
+++ Package.swift
@@ -18,9 +18,14 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "MyLibrary"),
+            name: "MyLibrary",
+            dependencies: [
+                .product(name: "NIOCore", package: "swift-nio")
+            ]),
         .testTarget(
             name: "MyLibraryTests",
-            dependencies: ["MyLibrary"]),
+            dependencies: [
+                "MyLibrary"
+            ]),
     ]
 )
