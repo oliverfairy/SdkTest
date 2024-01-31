db835b03a4d936618d7887402c8ba2fcec68b42e
Oliver An <131226876+oliverfairy@users.noreply.github.com>
2024-01-31 14:51:24 +0900

    Dependency added

--- Package.swift
+++ Package.swift
@@ -11,7 +11,9 @@ let package = Package(
             name: "MyLibrary",
             targets: ["MyLibrary"]),
     ],
-    dependencies: [],
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-nio.git", .upToNextMajor(from: "2.63.0")),
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
